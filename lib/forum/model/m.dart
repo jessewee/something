9d2d8ca5aaf8cd9e3b972b0ad2ac8826facf05b5
weisj <weisj@meixing.com>
2020-11-05 11:35:12 +0800

    []

--- lib/forum/model/m.dart
+++ lib/forum/model/m.dart
@@ -1,28 +1,29 @@
 import '../../common/models.dart';
 
 /// 社区用户信息
-class ForumUser {
-  final String id;
-  final String name;
-  final String avatar;
-  final String avatarThumb;
-  final Gender gender;
-  final String birthday;
-  final String registerDate;
+class ForumUser extends User {
   int followerCount; // 关注此用户的人数
   int followingCount; // 被此用户关注的人数
   bool followed; // 登录人是否已关注此用户
 
   ForumUser({
-    this.id = '',
-    this.name = '',
-    this.avatar = '',
-    this.avatarThumb = '',
-    this.gender = Gender.unknown,
-    this.birthday = '',
-    this.registerDate = '',
+    String id = '',
+    String name = '',
+    String avatar = '',
+    String avatarThumb = '',
+    Gender gender = Gender.unknown,
+    String birthday = '',
+    String registerDate = '',
     this.followerCount = 0,
     this.followingCount = 0,
     this.followed = false,
-  });
+  }) : super(
+          id: id,
+          name: name,
+          avatar: avatar,
+          avatarThumb: avatarThumb,
+          gender: gender,
+          birthday: birthday,
+          registerDate: registerDate,
+        );
 }
